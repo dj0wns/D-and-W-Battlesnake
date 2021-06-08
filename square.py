4c0736ac49442578fd3c8b61da7924a977958058
Waverly <waverlyroeger@gmail.com>
2021-06-07 22:28:34 -0400

    Correct and improve Square class.

--- square.py
+++ square.py
@@ -1,65 +1,66 @@
 class Square:
 
-  def __init__():
+  def __init__(self):
     self.set_empty()
     self.snake_distances = {}
 
   # OCCUPANT INFO
-  def set_empty():
+  def set_empty(self):
     self.contains_food = False
     self.contains_snake = None
     self.contains_snake_head = False
-    self.distance_to_empty = 0
+    self.distance_to_vacant = 0
 
-  def set_contains_food():
+  def set_contains_food(self):
     self.contains_food = True
     self.contains_snake = None
     self.contains_snake_head = False
-    self.distance_to_empty = 0
+    self.distance_to_vacant = 0
 
-  def set_contains_snake(snake_id, distance_to_empty):
+  def set_contains_snake(self, snake_id, distance_to_vacant):
     self.contains_food = False
     self.contains_snake = snake_id
     self.contains_snake_head = False
-    self.distance_to_empty = distance_to_empty
+    self.distance_to_vacant = distance_to_vacant
 
-  def set_contains_snake_head(snake_id, distance_to_empty):
+  def set_contains_snake_head(self, snake_id, distance_to_vacant):
     self.contains_food = False
     self.contains_snake = snake_id
     self.contains_snake_head = True
-    self.distance_to_empty = distance_to_empty
+    self.distance_to_vacant = distance_to_vacant
 
-  # DISTANCE TO EMPTY
-  def increment_distance_to_empty():
-    self.distance_to_empty += 1
+  # DISTANCE TO VACANT
+  def increment_distance_to_vacant(self):
+    self.distance_to_vacant += 1
 
-  def decrement_distance_to_empty():
-    if self.distance_to_empty > 0:
-      self.distance_to_empty -= 1
+  def decrement_distance_to_vacant(self):
+    if self.distance_to_vacant > 0:
+      self.distance_to_vacant -= 1
+
+  def is_empty(self):
+    return not (self.contains_food or self.contains_snake)
 
   # SNAKE DISTANCES
-  def set_distance_to_snake(snake_id, distance_to_snake):
+  def set_snake_distance(self, snake_id, distance_to_snake):
     self.snake_distances[snake_id] = distance_to_snake
 
-  def clear_distance_to_snake(snake_id):
+  def clear_snake_distance(self, snake_id):
     if snake_id in self.snake_distances.keys():
       del self.snake_distances[snake_id]
 
-  def clear_snake_distances():
+  def clear_snake_distances(self):
     self.snake_distances.clear()
 
-  def get_distance_to_snake(snake_id):
-    if snake_id in self.snake_distances.keys():
-      return self.snake_distances[snake_id]
-    else
-      return None
+  def get_snake_distance(self, snake_id):
+    return self.snake_distances[snake_id] \
+        if snake_id in self.snake_distances.keys() else None
 
-  def get_closest_snake():
+  def get_closest_snake(self):
     closest_snake = None
     closest_distance = None
     for snake_id in self.snake_distances.keys():
       distance = self.snake_distances[snake_id]
       if closest_distance is None or distance < closest_distance:
         closest_snake = snake_id
-        distance = closest_distance
-    return snake_id
+        closest_distance = distance
+    return closest_snake
